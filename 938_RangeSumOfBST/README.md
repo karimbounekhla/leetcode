7b9e8f05ac0e1c7dd667982b501840ab431a79f8
karimbounekhla <karim.bounekhla@mail.utoronto.ca>
2019-11-18 23:02:25 -0700

    938 - Range Sum of BST

---
+++ 938_RangeSumOfBST/README.md
@@ -0,0 +1,9 @@
+# 938. Range Sum of BST
+
+https://leetcode.com/problems/range-sum-of-bst/
+
+![image](image.png)
+
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Range Sum of BST.
+
+Memory Usage: 46 MB, less than 96.51% of Java online submissions for Range Sum of BST.
---
+++ 938_RangeSumOfBST/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        // Base Case
+        if (root == null) {
+            return 0;
+        }
+        
+        // Check if root is between L and R
+        if (root.val >= L && root.val <= R) {
+            return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+            
+        // Discard left side of tree if root value is less than L
+        } else if (root.val < L) {
+            return rangeSumBST(root.right, L, R);
+        } else {
+            return rangeSumBST(root.left, L, R);
+        }
+    }
+}
